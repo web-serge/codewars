b4051e5d71df585cd5e6b229395d78a60383737c
web-serge <146648233+web-serge@users.noreply.github.com>
2023-10-02 17:46:33 +0300

    'Calculate BMI' completed

--- 8kyu.js
+++ 8kyu.js
@@ -2,3 +2,9 @@
 function rentalCarCost(d) {
   return d * 40 - (d >=7 ? 50 : d >=3 ? 20 : 0)
 }
+
+// Calculate BMI
+function bmi(weight, height) {
+  const bmi = weight / height ** 2
+  return bmi <= 18.5 ? 'Underweight' : bmi <= 25.0 ? 'Normal' : bmi <= 30.0 ? 'Overweight' : bmi > 30 ? 'Obese' : null
+}
