49e673e6e052fa662259899b1b295ea719dc1f05
web-serge <146648233+web-serge@users.noreply.github.com>
2023-10-03 19:49:53 +0300

    'Ones and Zeros' completed

--- 7kyu.js
+++ 7kyu.js
@@ -2,3 +2,8 @@
 function reverseWords(str) {
   return str.split(' ').map(el => el.split('').reverse().join('')).join(' ')
 }
+
+// Ones and Zeros
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(""), 2)
+};
