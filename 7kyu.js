8dc44f81e7a8c136045d8e1394e5879d406f1364
Sergey Churekov <web_serge@outlook.com>
2023-10-14 11:09:43 +0300

    'Testing 1-2-3' completed

--- 7kyu.js
+++ 7kyu.js
@@ -1,39 +1,40 @@
 // Reverse words
 function reverseWords(str) {
-  return str.split(' ').map(el => el.split('').reverse().join('')).join(' ')
+    return str.split(' ').map(el => el.split('').reverse().join('')).join(' ')
 }
 
 // Ones and Zeros
 const binaryArrayToNumber = arr => {
-  return parseInt(arr.join(""), 2)
+    return parseInt(arr.join(""), 2)
 };
 
 // Descending Order
-function descendingOrder(n){
-  return Number(n.toString().split('').sort((a,b) => b - a).join(''))
+function descendingOrder(n) {
+    return Number(n.toString().split('').sort((a, b) => b - a).join(''))
 }
 
 // Is this a triangle?
-function isTriangle(a,b,c)
-{
+function isTriangle(a, b, c) {
     if (a + b > c && a + c > b && b + c > a) {
-     return true
-   } else {return false}
+        return true
+    } else {
+        return false
+    }
 }
 
 // Highest and Lowest
-function highAndLow(numbers){
-  let a = numbers.split(' ').sort( (a,b) => a - b)
-  return (a[a.length-1]+ ' ' +a[0])
+function highAndLow(numbers) {
+    let a = numbers.split(' ').sort((a, b) => a - b)
+    return (a[a.length - 1] + ' ' + a[0])
 }
 
 // Categorize New Member
-function openOrSenior(data){
-  const arr = []
-  data.forEach(el => {
-    el[0] >= 55 && el[1] > 7 ? arr.push('Senior') : arr.push('Open')
-  })
-  return arr
+function openOrSenior(data) {
+    const arr = []
+    data.forEach(el => {
+        el[0] >= 55 && el[1] > 7 ? arr.push('Senior') : arr.push('Open')
+    })
+    return arr
 }
 
 // Vowel Count
@@ -46,6 +47,13 @@ function getCount(str) {
 // Odd or Even?
 function oddOrEven(array) {
     let count = 0
-    array.forEach(i => { return count += i })
+    array.forEach(i => {
+        return count += i
+    })
     return count % 2 === 0 ? 'even' : 'odd'
+}
+
+// Testing 1-2-3
+let number = function (array) {
+    return array.map((el, index) => `${index + 1}: ${el}`)
 }
