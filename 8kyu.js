d73df934cc4a52aba066a34bf541f99361e707c8
Sergey Churekov <web_serge@outlook.com>
2023-10-17 21:06:59 +0300

    'altERnaTIng cAsE <=> ALTerNAtiNG CaSe' completed

--- 8kyu.js
+++ 8kyu.js
@@ -37,3 +37,15 @@ function switchItUp(number) {
 function removeEveryOther(arr) {
     return arr.filter((item, index) => index % 2 === 0)
 }
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+String.prototype.toAlternatingCase = function () {
+  let result = ''
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === this[i].toUpperCase()){
+      result += this[i].toLowerCase()
+    } else {
+      result += this[i].toUpperCase()
+    }
+  }
+  return result
+}
