3b57ed29c1d5dffa0b5cac0567b81b7b9b8eed8e
Sergey Churekov <web_serge@outlook.com>
2023-10-27 21:23:29 +0300

    'Count the smiley faces!' completed

--- 6kyu.js
+++ 6kyu.js
@@ -7,3 +7,15 @@ function solution(string) {
         return i
     }).join('')
 }
+// Count the smiley faces!
+function countSmileys(arr) {
+  const result = []
+  arr.forEach(el => {
+    if (el.length === 2 && (el[0] === ':' || el[0] === ';') &&( el[1] === 'D' || el[1] === ')')) {
+      result.push(el)
+    } else if (el.length === 3 && (el[0] === ':' || el[0] === ';') && (el[1] === '~' || el[1] === '-')  && (el[2] === 'D' || el[2] === ')')) {
+      result.push(el)
+    } else return 0
+  })
+  return result.length
+}
