d229127abeffa2e0fba4f196673bbebf9f3a325f
Sergey Churekov <web_serge@outlook.com>
2023-11-07 23:12:57 +0300

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -129,3 +129,17 @@ function noBoringZeros(n) {
   }
   return Number(array.join(''))
 }
+// Count of positives / sum of negatives
+function countPositivesSumNegatives(input) {
+  let count = 0
+  let sum = 0
+  if (!input || input.length === 0) return []
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      count++
+    } else if (input[i] < 0) {
+      sum += input[i]
+    } 
+  }
+    return [count, sum]
+}
