03fad953df31a17f551d1425a98484745ab96a88
Sergey Churekov <web_serge@outlook.com>
2023-11-14 12:36:53 +0300

    Update 6kyu.js

--- 6kyu.js
+++ 6kyu.js
@@ -56,7 +56,7 @@ function findUniq(arr) {
     return result[0][0]
   } else return result[1][0]
 }
-// 
+// Two Sum
 function twoSum(numbers, target) {
     const result = []
     for (let i = 0; i < numbers.length; i++) {
@@ -69,4 +69,18 @@ function twoSum(numbers, target) {
         }
       }
     }
+}
+// Replace With Alphabet Position
+function alphabetPosition(text) {
+  const array = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  const result = []
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (text[i].toLowerCase() === array[j]) {
+        result.push(j+1)
+      }
+    }
+  }
+  console.log(result.join(' '))
+  return result.join(' ')
 }
