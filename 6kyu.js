ad102ad22ff4632296839489a0a0365c52869aba
Sergey Churekov <web_serge@outlook.com>
2023-11-18 20:57:43 +0300

    Update 6kyu.js

--- 6kyu.js
+++ 6kyu.js
@@ -97,3 +97,13 @@ function arrayDiff(a, b) {
   }
   return result
 }
+// Who likes it?
+function likes(names) {
+  switch (true) {
+      case names.length === 0: return 'no one likes this'
+      case names.length === 1: return `${names[0]} likes this`
+      case names.length === 2: return `${names[0]} and ${names[1]} like this`
+      case names.length === 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`
+      default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }
+}
