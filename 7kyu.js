c4d574bf8aa033ba12df3ed4260e86d2d0a9e717
Sergey Churekov <web_serge@outlook.com>
2024-02-28 21:06:17 +0300

    Update 7kyu.js

--- 7kyu.js
+++ 7kyu.js
@@ -340,3 +340,11 @@ function factorial(n){
   if (n === 0 || n === 1) return 1
   return n * factorial(n - 1)
 }
+// Maximum Product
+function adjacentElementsProduct(array) {
+  const res = []
+  array.forEach((el, idx) => {
+    res.push(el * array[idx + 1])
+  })
+  return res.sort((a,b)=>b-a)[0]
+}
